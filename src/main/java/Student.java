4810daf5c0ff5401db3ce6d1ba7ba961d0178932
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-10-30 18:18:08 +0000

    Added Code to Student Class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,34 @@
-public class Student {
-    
+//INHERITANCE - Jeremy Lu 10/28/2020
+public class Student extends Person {
+    //Instance Variables:
+    private int classyear;
+
+    //Constructors: [Come back to this LATER]
+    public Student(String a, String b, int c){
+        super(a , b);
+        // this.firstName = a;
+        // this.lastName = b;
+        this.classyear = c;
+    }
+
+    public Student() {
+        super("Jerry", "Seinfeld");
+        this.classyear = 2024;
+    }
+
+    public String toString(){
+        String str = this.getFirstName() + " " + this.getLastName() + " " + this.getclassyear();
+        return str;
+    }
+
+    //Getter:
+    public int getclassyear() {
+        return this.classyear;
+    }
+    //Setter:
+    public void setclassyear(int classyear) {
+        if(classyear > 2020 && classyear <= 2025){
+            this.classyear = classyear;
+        }
+    }
 }
--- src/main/java/StudentClient.java
+++ src/main/java/StudentClient.java
@@ -1,5 +1,6 @@
 public class StudentClient {
     public static void main(String[] args) {
-        
+        Student s1 = new Student("Bob", "Dylan", 2024);
+        System.out.println(s1);
     }
 }
